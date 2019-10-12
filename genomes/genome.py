f75a05ccd5edd7924a0311d9a939b9a1c9b39cf8
kyhoon <yh01dlx@gmail.com>
2019-10-12 08:17:27 +0000

    Add operations and Node class

--- genomes/genome.py
+++ genomes/genome.py
@@ -1,8 +1,32 @@
-import torch.nn as nn
-from genomes.operations import OPS_1D, OPS_2D
+import genomes.operations as ops
 
 
-class Genome:
-    def __init__(self, file):
-        # TODO:
-        # create different genome classes for CNN, RNN, Transformer
+class Node:
+    IDX_SEP = 'i'
+    OPERATIONS = {
+        '0': 'sep_conv',
+        '1': 'dil_conv',
+        '2': 'max_pool',
+        '3': 'avg_pool',
+        '4': 'self_attn',
+        '5': 'enc_attn',
+        '6': 'identity',
+        '7': 'none'
+    }
+    KERNEL_SIZE = {
+        '0': 1,
+        '1': 3,
+        '2': 5
+    }
+    ACTIVATIONS = {
+        '0': 'relu',
+        '1': 'sigmoid',
+        '2': 'tanh'
+    }
+
+    def __init__(self, seq):
+        idx1, seq = seq.split(self.IDX_SEP, 1)
+        ops1, seq = seq[:3], seq[3:]
+        idx2, ops2 = seq.split(self.IDX_SEP, 1)
+        self.idx1 = int(idx1)
+        self.idx2 = int(idx2)
--- genomes/operations.py
+++ genomes/operations.py
@@ -1,35 +1,15 @@
-import torch
 import torch.nn as nn
 
-OPS_1D = {
-    'none': lambda C: Zero(1),
-    'identity': lambda C: Identity(),
-    'tanh': lambda C: DenseTanh(C),
-    'relu': lambda C: DenseReLU(C),
-    'sigmoid': lambda C: DenseSigmoid(C),
-}
-
-OPS_2D = {
-    'none': lambda C, stride: Zero(stride),
-    'skip_connect': lambda C, stride: Identity() if stride == 1 else FactorizedReduce(C),
-    'sep_conv_3x3': lambda C, stride: SepConv(C, C, 3, stride, 1),
-    'sep_conv_5x5': lambda C, stride: SepConv(C, C, 5, stride, 2),
-    'sep_conv_7x7': lambda C, stride: SepConv(C, C, 7, stride, 3),
-    'dil_conv_3x3': lambda C, stride: DilConv(C, C, 3, stride, 2, 2),
-    'dil_conv_5x5': lambda C, stride: DilConv(C, C, 5, stride, 4, 2),
-    'avg_pool_3x3': lambda C, stride: nn.AvgPool2d(3, stride=stride, padding=1, count_include_pad=False),
-    'max_pool_3x3': lambda C, stride: nn.MaxPool2d(3, stride=stride, padding=1),
-}
+__all__ = ['none', 'identity']
+__all__ += ['sep_conv', 'dil_conv', 'max_pool', 'avg_pool']
+__all__ += ['attention']
 
 
 class Zero(nn.Module):
-    def __init__(self, stride):
+    def __init__(self):
         super().__init__()
-        self.stride = stride
 
     def forward(self, x):
-        if self.stride != 1:
-            x = x[:, :, ::self.stride, ::self.stride]
         return x.mul(0.)
 
 
@@ -41,74 +21,64 @@ class Identity(nn.Module):
         return x
 
 
-class DenseTanh(nn.Module):
-    def __init__(self, C):
-        super().__init__()
-        self.linear = nn.Linear(C, 2*C)
-
-    def forward(self, x):
-        return torch.tanh(self.linear(x))
+def identity(D, C, ks):
+    return Identity()
 
 
-class DenseReLU(nn.Module):
-    def __init__(self, C):
-        super().__init__()
-        self.linear = nn.Linear(C, 2*C)
-
-    def forward(self, x):
-        return torch.relu(self.linear(x))
+def none(D, C, ks):
+    return Zero()
 
 
-class DenseSigmoid(nn.Module):
-    def __init__(self, C):
-        super().__init__()
-        self.linear = nn.Linear(C, 2*C)
-
-    def forward(self, x):
-        return torch.sigmoid(self.linear(x))
-
-
-class FactorizedReduce(nn.Module):
-    def __init__(self, C):
-        super().__init__()
-        assert C % 2 == 0
-        self.conv_1 = nn.Conv2d(C, C // 2, 1, stride=2, padding=0, bias=False)
-        self.conv_2 = nn.Conv2d(C, C // 2, 1, stride=2, padding=0, bias=False)
-        self.bn = nn.BatchNorm2d(C)
-
-    def forward(self, x):
-        out = torch.cat([self.conv_1(x), self.conv_2(x[:, :, 1:, 1:])], dim=1)
-        out = torch.relu(self.bn(out))
-        return out
-
-
-class DilConv(nn.Module):
-    def __init__(self, C, kernel_size, stride, padding, dilation):
-        super().__init__()
-        self.op = nn.Sequential(
-            nn.Conv2d(C, C, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=C, bias=False),
-            nn.Conv2d(C, C, kernel_size=1, padding=0, bias=False),
-            nn.BatchNorm2d(C),
-            nn.ReLU(inplace=False)
+def sep_conv(D, C, ks):
+    if D == 1:
+        return nn.Sequential(
+            nn.Conv1d(C, C, ks, padding=ks//2, groups=C),
+            nn.Conv1d(C, C, 1, padding=0)
         )
-
-    def forward(self, x):
-        return self.op(x)
-
-
-class SepConv(nn.Module):
-    def __init__(self, C, kernel_size, stride, padding):
-        super().__init__()
-        self.op = nn.Sequential(
-            nn.Conv2d(C, C, kernel_size=kernel_size, stride=stride, padding=padding, groups=C, bias=False),
-            nn.Conv2d(C, C, kernel_size=1, padding=0, bias=False),
-            nn.BatchNorm2d(C),
-            nn.ReLU(inplace=False),
-            nn.Conv2d(C, C, kernel_size=kernel_size, stride=1, padding=padding, groups=C, bias=False),
-            nn.Conv2d(C, C, kernel_size=1, padding=0, bias=False),
-            nn.BatchNorm2d(C),
-            nn.ReLU(inplace=False)
+    elif D == 2:
+        return nn.Sequential(
+            nn.Conv2d(C, C, ks, padding=ks//2, groups=C),
+            nn.Conv2d(C, C, 1, padding=0)
         )
+    else:
+        raise NotImplementedError
 
-    def forward(self, x):
-        return self.op(x)
+
+def dil_conv(D, C, ks):
+    if D == 1:
+        return nn.Sequential(
+            nn.Conv1d(C, C, ks, padding=ks - 1, dilation=2, groups=C),
+            nn.Conv1d(C, C, 1, padding=0)
+        )
+    elif D == 2:
+        return nn.Sequential(
+            nn.Conv2d(C, C, ks, padding=ks - 1, dilation=2, groups=C),
+            nn.Conv2d(C, C, 1, padding=0)
+        )
+    else:
+        raise NotImplementedError
+
+
+def max_pool(D, C, ks):
+    if D == 1:
+        return nn.MaxPool1d(ks, padding=ks//2)
+    elif D == 2:
+        return nn.MaxPool2d(ks, padding=ks//2)
+    else:
+        raise NotImplementedError
+
+
+def avg_pool(D, C, ks):
+    if D == 1:
+        return nn.AvgPool1d(ks, padding=ks//2, count_include_pad=False)
+    elif D == 2:
+        return nn.AvgPool2d(ks, padding=ks//2, count_include_pad=False)
+    else:
+        raise NotImplementedError
+
+
+def attention(D, C, ks):
+    if D == 1:
+        return nn.MultiHeadAttention(C, 8)
+    else:
+        raise NotImplementedError
